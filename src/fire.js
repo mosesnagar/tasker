9c682f33258ec135b48c1d07d98c06f40134e477
mosesnagar <mosesnagar@gmail.com>
2019-02-11 19:48:55 +0200

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyB2Y3TsBS4FTBYkzxamKnG3G43ixCrNQ0M",
-    authDomain: "tasker-1111.firebaseapp.com",
-    databaseURL: "https://tasker-1111.firebaseio.com",
-    projectId: "tasker-1111",
-    storageBucket: "tasker-1111.appspot.com",
-    messagingSenderId: "663896874239"
+    apiKey: "##########",
+    authDomain: ""##########",",
+    databaseURL: ""##########",",
+    projectId: ""##########",",
+    storageBucket: ""##########",",
+    messagingSenderId: ""##########","
 };
 
 const fire = firebase.initializeApp(config);
